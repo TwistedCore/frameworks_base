ba68c8fa59e50a9ff82050bd96703d0067242060
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-11-27 14:09:18 -0500

    Port "Option to hide Adb notification icon (2/2)"
    Change-Id: I2a6c5eed646c4a2121890201de2a7b8cca7f1b1a

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6368,6 +6368,17 @@ public final class Settings {
         public static final String WEB_ACTION_ENABLED = "web_action_enabled";
 
         /**
+         * Beginning of Flash Secure Settings Additions
+         * @hide
+         */
+
+        /**
+         * Whether to display the ADB notification.
+         * @hide
+         */
+        public static final String ADB_NOTIFY = "adb_notify";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear

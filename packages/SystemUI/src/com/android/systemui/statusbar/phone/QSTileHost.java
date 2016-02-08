0115973f3b016399652b68fd7fa52c378fbeb090
LorDClockaN <davor@losinj.com>
2016-02-08 15:08:02 +0100

    QS: App picker Tile
    Change-Id: I2b47261ce757b59677fcea5f5a133ab8ecbcade9
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    (cherry picked from commit 26b3211509539b4a2495c54c4a1ad1c1b5047b25)
    
    # Conflicts:
    #	packages/SystemUI/AndroidManifest.xml
    #	packages/SystemUI/res/values/config.xml
    #	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -43,6 +43,7 @@ import com.android.systemui.qs.external.TileServices;
 import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.AndroidAutoTile;
+import com.android.systemui.qs.tiles.AppPickerTile;
 import com.android.systemui.qs.tiles.BatteryTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.BrightnessTile;
@@ -461,6 +462,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("heads_up")) return new HeadsUpTile(this);
         else if (tileSpec.equals("androidauto")) return new AndroidAutoTile(this);
         else if (tileSpec.equals("navigation_bar")) return new NavigationBarTile(this);
+        else if (tileSpec.equals("app_picker")) return new AppPickerTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else if (tileSpec.startsWith(CustomTile.PREFIX)) return CustomTile.create(this,tileSpec);

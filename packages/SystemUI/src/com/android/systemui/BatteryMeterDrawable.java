d2be06c16ae64f70587b9288a9b25e72b8373fe6
Willi Ye <williye97@gmail.com>
2016-06-10 14:41:59 +0200

    circlebattery: bring back pulsing animation
    @xanaxdroid forward port to 7.1.1
    
    Change-Id: I351c48508bb160464d03ce2aab5dbeb1fd7e3f78
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    (cherry picked from commit 71e50ff03d083b7e623d586902322edb7e0aa740)

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -16,7 +16,10 @@
 
 package com.android.systemui;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.annotation.Nullable;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -136,6 +139,7 @@ public class BatteryMeterDrawable extends Drawable implements
     private Drawable mFrameDrawable;
     private StopMotionVectorDrawable mLevelDrawable;
     private Drawable mBoltDrawable;
+    private ValueAnimator mAnimator;
 
     private int mTextGravity;
 
@@ -278,7 +282,10 @@ public class BatteryMeterDrawable extends Drawable implements
     public void onBatteryLevelChanged(int level, boolean pluggedIn, boolean charging) {
         mLevel = level;
         mPluggedIn = pluggedIn;
-
+        if ((Settings.Secure.getInt(mContext.getContentResolver(),
+                 Settings.Secure.STATUS_BAR_BATTERY_STYLE, 0) == 2)) {
+            animateCircleBattery(level, pluggedIn, charging);
+        }
         postInvalidate();
     }
 
@@ -375,6 +382,37 @@ public class BatteryMeterDrawable extends Drawable implements
             }
         }
         return color;
+    }
+
+    public void animateCircleBattery(int level, boolean pluggedIn, boolean charging) {
+        if (charging) {
+            if (mAnimator != null) mAnimator.cancel();
+
+            final int defaultAlpha = mLevelDrawable.getAlpha();
+            mAnimator = ValueAnimator.ofInt(defaultAlpha, 0, defaultAlpha);
+            mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    mLevelDrawable.setAlpha((int) animation.getAnimatedValue());
+                    invalidateSelf();
+                }
+            });
+            mAnimator.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                    mLevelDrawable.setAlpha(defaultAlpha);
+                    mAnimator = null;
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLevelDrawable.setAlpha(defaultAlpha);
+                    mAnimator = null;
+                }
+            });
+            mAnimator.setDuration(2000);
+            mAnimator.start();
+        }
     }
 
     public void setDarkIntensity(float darkIntensity) {

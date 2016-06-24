eab85b7d3b9bbda9eb6b6f810c5706fd9a721a51
Alex Cruz <mazdarider23@gmail.com>
2016-06-23 21:31:41 -0400

    Lockscreen Clock Widget Fonts [1/2] Squashed with: *Lockscreen fonts: set correct default value -@BeansTown106 *Update Lockscreen clock font option -@BeansTown106 *fix lockscreen font resetting on reboot -@BeansTown106
    PureNexus Edits:
    *Squashed
    *Modified for use with our observer
    *Cleaned up if statements a bit saved about 15 LOC
    
    # Conflicts:
    #	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3747,6 +3747,12 @@ public final class Settings {
         public static final String SHOW_LOCKSCREEN_DATE = "show_lockscreen_date";
 
         /**
+         * Change fonts for the system lockscreen clock widget
+         * @hide
+         */
+        public static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

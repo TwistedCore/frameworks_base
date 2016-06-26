5aa59a06ff48c3bd7c54052cf67c3231fbee92ea
LorDClockaN <davor@losinj.com>
2016-06-26 15:29:49 +0200

    Toast icon switch [1/2]
    (cherry picked from commit b2271e3b2df4177b19596f83cae82457c25accfa)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3931,6 +3931,13 @@ public final class Settings {
         public static final String QS_DATE_TIME_CENTER = "qs_date_time_center";
 
         /**
+         * Toast icon
+         *
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

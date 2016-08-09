9bdd023397366c53b9d9256208e577ef1122a7e9
Corey Edwards <ensabahnur16@gmail.com>
2016-08-09 09:07:17 -0500

    Enable\Disable bluetooth battery status[1/2]
    Based on this patch:
    http://review.cyanogenmod.org/#/c/153034/
    https://github.com/CyanogenMod/android_frameworks_base/commit/88e7a6c7fcaf71d6dae2b9067db19aaeba7de39b
    
    This just makes it configurable for the user, in case they don't want to see that, or if their themes doesn't support the new icons yet.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3791,6 +3791,12 @@ public final class Settings {
         public static final String QS_SHOW_BRIGHTNESS_ICON = "qs_show_brightness_icon";
 
         /**
+         * Enable\Disable Bluetooth Battery bar
+         * @hide
+         */
+        public static final String BLUETOOTH_SHOW_BATTERY = "bluetooth_show_battery";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

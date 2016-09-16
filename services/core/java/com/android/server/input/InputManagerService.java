c14083f84cd1d848bdb949104aa3683ae117a951
Branden M <wasabi.dev@gmail.com>
2016-09-16 00:53:14 -0500

    Fix conflict error

--- services/core/java/com/android/server/input/InputManagerService.java
+++ services/core/java/com/android/server/input/InputManagerService.java
@@ -561,15 +561,6 @@ public class InputManagerService extends IInputManager.Stub
         }
     }
 
-    private int findInputFilterIndexLocked(IInputFilter filter) {
-        for (int i = 0; i < mInputFilterChain.size(); i++) {
-            if (mInputFilterChain.get(i).mInputFilter == filter) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
     @Override // Binder call
     public boolean injectInputEvent(InputEvent event, int mode) {
         return injectInputEventInternal(event, Display.DEFAULT_DISPLAY, mode);

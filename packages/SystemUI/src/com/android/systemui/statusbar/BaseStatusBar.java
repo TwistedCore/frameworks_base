1a481c85d49c74c0c30b7893a8c1c2ac3e5c8632
Marc K <morckx@gmail.com>
2016-10-19 22:26:28 +0200

    Add new importance level for blocking lock screen notifications (1/2)
    One feature that is still missing in the handling of notifications in
    N is the possibility to block them app-wise for relevance/importance
    reasons rather than for privacy reasons – i.e. also on the insecure
    lock screen.
    
    This patch plugs the hole by introducing a new importance level "very
    low" between "min" and "low" in the power notification controls that
    allows notifications on the status bar, but not on the lock screen.
    
    It brings back most of the functionality of
    Ib166db1b1673aeaea132c8eeb16c650d2f254a82 excluding the ability of
    blocking only ongoing (persistent) notifications.
    
    Change-Id: I5ceee18a3e9b507ddb51a224eddda8b38957ec75
    Signed-off-by: Marc K <morckx@gmail.com>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    # Conflicts:
    #	packages/SystemUI/res/layout/power_notification_controls_settings.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -126,6 +126,7 @@ import java.util.Locale;
 import java.util.Set;
 
 import static android.service.notification.NotificationListenerService.Ranking.IMPORTANCE_HIGH;
+import static android.service.notification.NotificationListenerService.Ranking.IMPORTANCE_VERY_LOW;
 
 public abstract class BaseStatusBar extends SystemUI implements
         CommandQueue.Callbacks, ActivatableNotificationView.OnActivatedListener,
@@ -2410,7 +2411,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     public boolean shouldShowOnKeyguard(StatusBarNotification sbn) {
         return mShowLockscreenNotifications && !mNotificationData.isAmbient(sbn.getKey())
-            && mNotificationData.getVisibilityOverride(sbn.getKey()) != Notification.VISIBILITY_SECRET;
+            && mNotificationData.getImportance(sbn.getKey()) > IMPORTANCE_VERY_LOW;
     }
 
     protected void setZenMode(int mode) {

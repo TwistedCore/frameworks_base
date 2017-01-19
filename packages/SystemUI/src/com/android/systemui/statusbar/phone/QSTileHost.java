319c24232ab1e354cfb13b5ae5dd2876e3e5d405
ezio84 <brabus84@gmail.com>
2017-01-19 11:55:57 +0100

    Revert CM nfc tile - now aosp has its official one
    i'm keeping nfc permissions in the sysui manifest just
    for convenience
    
    (cherry picked from commit 799b9dc9cc529fde18528c8ad4ed0d807b83db2e)
    
    # Conflicts:
    #	packages/SystemUI/res/drawable/ic_qs_nfc_off.xml
    #	packages/SystemUI/res/drawable/ic_qs_nfc_on.xml
    #	packages/SystemUI/res/values/config.xml
    #	packages/SystemUI/res/values/custom_strings.xml
    #	packages/SystemUI/src/com/android/systemui/qs/tiles/NfcTile.java
    #	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -62,7 +62,6 @@ import com.android.systemui.qs.tiles.ImeTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MusicTile;
-import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.NavigationBarTile;
 import com.android.systemui.qs.tiles.RebootTile;
